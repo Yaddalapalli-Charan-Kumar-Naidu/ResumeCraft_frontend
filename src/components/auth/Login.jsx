44a479139475e58a7e86cf61177b6dbcddce7e00
Yaddalapalli-Charan-Kumar-Naidu <charankumarnaidu2004@gmail.com>
2025-01-14 14:43:21 +0530

    login backend integration completed

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -9,11 +9,12 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { Container } from "@mui/material";
-
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 export default function Login() {
-  const BASEURL="http://localhost:8267"
-
+  // const BASEURL="http://localhost:8267"
+  const BASEURL=import.meta.env.VITE_BASEURL;
+  const navigate=useNavigate();
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -30,6 +31,8 @@ export default function Login() {
     axios.request(config)
     .then((response) => {
       console.log(JSON.stringify(response.data));
+      localStorage.setItem("token",response.data.token);
+      navigate("/");
     })
     .catch((error) => {
       console.log(error);
